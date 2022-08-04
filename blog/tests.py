f5417e7c1f456eae43766ddc1ec01b7db2c047cc
Ludwig Ahn <2045sky@naver.com>
2022-08-04 20:01:02 +0900

    내비게이션 바와 푸터 모델화하기

--- blog/tests.py
+++ blog/tests.py
@@ -11,6 +11,23 @@ class TestView(TestCase):
 		self.client=Client()
 
 
+	def navbar_test(self,soup):
+		navbar=soup.nav
+		self.assertIn('Blog',navbar.text)
+		self.assertIn('About Me',navbar.text)
+
+
+		logo_btn=navbar.find('a',text='Do it Django')
+		self.assertEqual(logo_btn.attrs['href'],'/')
+		home_btn=navbar.find('a',text='Home')
+		self.assertEqual(home_btn.attrs['href'],'/')
+		blog_btn=navbar.find('a',text='Blog')
+		self.assertEqual(blog_btn.attrs['href'],'/blog/')
+		about_me_btn=navbar.find('a',text='About Me')
+		self.assertEqual(about_me_btn.attrs['href'],'/about_me/')
+		
+
+
 	def test_post_list(self):
 		#1.1 포스트 목록 페이지를 가져온다
 		response = self.client.get('/blog/')
@@ -19,12 +36,8 @@ class TestView(TestCase):
 		#1.3 페이지 타이틀은 'Blog'
 		soup=BeautifulSoup(response.content, 'html.parser')
 		self.assertEqual(soup.title.text, 'Blog\n')
-		#1.4 내비게이션 바가 있다
-		navbar=soup.nav
-		#1.5 Blog, About Me라는 문구가 내비게이션 바에 있다.
-		self.assertIn('Blog',navbar.text)
-		self.assertIn('About Me',navbar.text)
-
+		
+		
 		#2.1 메인 영역에 게시물이 하나도 없다면
 		self.assertEqual(Post.objects.count(), 0)
 		#2.2 '아직 게시물이 없습니다'
@@ -52,6 +65,8 @@ class TestView(TestCase):
 		self.assertIn(post_002.title, main_area.text)
 		#3.4 '아직 게시물이 없습니다' 라는 문구는 더 이상 나타나지 않는다.
 		self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+		#네비게이션 바 테스트
+		self.navbar_test(soup)
 
 
 	def test_post_detail(self):
@@ -83,4 +98,5 @@ class TestView(TestCase):
 
 		#2.6 첫 번째 포스트의 내용이 포스트 영역에 있다.
 		self.assertIn(post_001.content,post_area.text)
-		
+		#네비게이션 바 테스트
+		self.navbar_test(soup)
