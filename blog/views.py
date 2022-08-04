70f9de13d340bad80cbc158d0b274a95010b7129
Ludwig Ahn <2045sky@naver.com>
2022-08-05 00:06:40 +0900

    테스트 불량

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from msilib.schema import ListView
 from turtle import pos
+from unicodedata import category
 #from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 
 
 # Create your views here.
@@ -11,6 +12,19 @@ class PostList(ListView):
 	ordering= '-pk'
 
 
+	def get_context_data(self, **kwargs):
+		context=super(PostList,self).get_context_data()
+		context['categories']=Category.objects.all()
+		context['no_category_post_count']=Post.objects.filter(category=None).count()
+		return context
+
+
 class PostDetail(DetailView):
 	model=Post
 
+
+	def get_context_data(self, **kwargs):
+		context=super(PostDetail,self).get_context_data()
+		context['categories']=Category.objects.all()
+		context['no_category_post_count']=Post.objects.filter(category=None).count()
+		return context
